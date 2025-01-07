58a7efe52c1da0a1e088e6fe104d515db60d57e5
Shashank <shashank.agarwal12@gmail.com>
2025-01-07 08:07:18 +0530

    Initial commit for Angular v14 project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/code-angular-v14'),
+      dir: require('path').join(__dirname, './coverage/ng14-workspace'),
       subdir: '.',
       reporters: [
         { type: 'html' },
