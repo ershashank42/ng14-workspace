29730198054398df5055fed7d772a6aed3fc231e
Agarwal <267245@ust.com>
2025-04-29 14:18:14 +0530

    Observable example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DataService } from './services/data-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng14-workspace';
+  subscription: Subscription;
+  message: string = '';
+  counter: number = 0;
+
+  constructor(private dataService: DataService) {
+    this.subscription = this.dataService.getData().subscribe({
+      next: (value) => {
+        this.message = value;
+      }
+    });
+  }
+
+  sendData() {
+    this.dataService.emitData("New data: " + this.counter);
+    this.counter++;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
